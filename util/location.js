d17219118b833607be60226f957214a8361357d9
unknown <nowakpawel99@interia.pl>
2022-12-17 00:17:57 +0100

    configuration vercel

--- util/location.js
+++ util/location.js
@@ -9,11 +9,11 @@ async function getCoordsForAddress(address) {
   //   lat: 40.7484474,
   //   lng: -73.9871516
   // };
-  const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-      address
-    )}&key=${API_KEY}`
-  );
+  // const response = await axios.get(
+  //   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+  //     address
+  //   )}&key=${API_KEY}`
+  // );
 
   const data = response.data;
 
