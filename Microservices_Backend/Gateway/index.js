808ac9d3649cf7a13ceb5d6e5155fa8eb057d4f9
unknown <nowakpawel99@interia.pl>
2023-01-04 08:55:05 +0100

    gateway testing

--- Microservices_Backend/Gateway/index.js
+++ Microservices_Backend/Gateway/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const proxy = require('express-http-proxy');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/places', proxy('http://localhost:8004'))
 app.use('/api/routes', proxy('http://localhost:8002'))
-app.use('/api/users', proxy('http://localhost:8003'))
+// app.use('/api/users', proxy('http://localhost:8003'))
 
 
 app.listen(8000, () => {
